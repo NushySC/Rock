e57c1df61b30a8808f097b2875985f6501b770a3
NushySC <ank.sampedro@gmail.com>
2019-12-16 17:02:15 +0100

    filtering

--- js/index.js
+++ js/index.js
@@ -9,3 +9,27 @@ $(function(){
         $(".hamburger").toggleClass("is-active");
     });
 })
+
+$(function(){
+    $(".btn--art").click(function(){
+        $(".art").toggleClass("hide");
+    });
+})
+
+$(function(){
+    $(".btn--music").click(function(){
+        $(".music").toggleClass("hide");
+    });
+})
+
+$(function(){
+    $(".btn--theatre").click(function(){
+        $(".theatre").toggleClass("hide");
+    });
+})
+
+$(function(){
+    $(".btn--poetry").click(function(){
+        $(".poetry").toggleClass("hide");
+    });
+})
